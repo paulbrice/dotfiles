c8575e25d1023ec40935a5e0955f8ca08d7db5bc
Paul Brice <34553382+paulbrice@users.noreply.github.com>
2018-08-21 10:52:21 -0600

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-code --install-extension ms-python.python
-code --install-extension PeterJausovec.vscode-docker
-code --install-extension mauve.terraform
-code --install-extension lukehoban.go
-code --install-extension ms-vscode.powershell
 code --install-extension aws-scripting-guy.cform
-code --install-extension DavidAnson.vscode-markdownlint
-code --install-extension DotJoshJohnson.xml
+code --install-extension danielthielking.aws-cloudformation-yaml
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension dotjoshjohnson.xml
+code --install-extension korekontrol.saltstack
+code --install-extension mauve.terraform
 code --install-extension mohsen1.prettify-json
-code --install-extension ms-vscode.Go
+code --install-extension ms-python.python
+code --install-extension ms-vscode.go
+code --install-extension pendrica.chef
+code --install-extension peterjausovec.vscode-docker
+code --install-extension redhat.vscode-yaml
+code --install-extension robertohuertasm.vscode-icons
